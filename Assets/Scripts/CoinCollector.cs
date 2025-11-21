329022978b1164bcf95811af3945de1af403d1f7
Kraftware650 <raslev06@gmail.com>
2025-11-21 16:18:28 +0100

    Added Sounds and a collectable coin prefab
    Collectable prefab och ljudeffekter

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -2,13 +2,18 @@ using UnityEngine;
 
 public class CoinCollector : MonoBehaviour
 {
+    [SerializeField] private TMPro.TextMeshProUGUI textBox;
+    [SerializeField] private AudioSource coinCollect;
     private int coinAmount = 0;
+    private string coinText = "Coins Collected: ";
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Coin"))
         {
             Destroy(other.gameObject);
             coinAmount++;
+            textBox.text = coinText + coinAmount;
+            coinCollect.Play();
         }
     }
 }
--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -7,7 +7,9 @@ public class Dialogue : MonoBehaviour
 {
     [SerializeField] private TMPro.TextMeshProUGUI textBox;
     [SerializeField] private Button nextButton;
+    [SerializeField] private AudioSource dialogueSound;
     [SerializeField] List<string> dialogue;
+
 
     [SerializeField] private UnityEvent onStartDialogue;
     [SerializeField] private UnityEvent onEndDialogue;
@@ -26,10 +28,12 @@ public class Dialogue : MonoBehaviour
         
         // Move to the next item in the list of dialogue-lines
         currentIndex++;
+        
         // If we're not at the end of the list yet: update text in text-box
         if (currentIndex < dialogue.Count)
         {
             textBox.text = dialogue[currentIndex];
+            dialogueSound.Play();
         }
         // If we are at the end of the list: run the onEndDialogue-event, reset the index (so that the dialogue can be restarted) and remove this dialog from the next-button
         else
